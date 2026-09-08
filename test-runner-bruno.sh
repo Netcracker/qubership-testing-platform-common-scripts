b27098890281870a724931f0ba01b5e5f1837b0c
arko0220 <artem.kostiuchik@netcracker.com>
2026-09-08 18:03:38 +0500

    feat: update conversion to allure

--- test-runner-bruno.sh
+++ test-runner-bruno.sh
@@ -61,7 +61,7 @@ run_bruno_from_test_params() {
   } > "$PATH_TO_ALLURE_RESULTS/environment.properties"
 
   # Export everything the subprocess needs (arrays can't cross fork; serialise folders).
-  export -f run_collection_body resolve_folders run_bru write_allure_placeholder wait_for_collection_slot \
+  export -f run_collection_body resolve_folders list_bru_run_targets run_bru write_allure_placeholder wait_for_collection_slot \
     bruno_collection_display_name bruno_collection_file_slug
 
   export TMP_DIR PROJECT_DIR PATH_TO_ATTACHMENTS_DIR PATH_TO_ALLURE_RESULTS
